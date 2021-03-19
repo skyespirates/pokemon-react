d7428bb2c06dc19b58733d08bf0f21efd2421efd
Skyes Crawford <akashiseijurou414@gmail.com>
2021-03-20 05:50:04 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./About";
+const url = "https://pokeapi.co/api/v2/pokemon?offset=0";
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then(({ results }) => setPokemons(results))
+      .catch((error) => console.log(error));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="bg-gray-100">
+        <h1 className="text-2xl bg-blue-100 border-2 border-solid border-purple-500 border-opacity-5">
+          Pokemons
+        </h1>
+        <div>
+          {pokemons.map((pokemon, idx) => (
+            <p className="border border-purple-500" key={idx}>
+              {pokemon.name}
+            </p>
+          ))}
+        </div>
+      </div>
+      <Switch>
+        <Route path="/about" component={About} />
+      </Switch>
+    </Router>
   );
 }
 
