943aa08832be9ec88597807a1f2e4cd299ae1fbc
Skyes Crawford <akashiseijurou414@gmail.com>
2021-03-21 05:58:09 +0700

    fetch data and add About component

--- src/About.js
+++ src/About.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-export default function About() {
+export default function About(props) {
+  let { slug } = useParams();
+  const [pokemon, setPokemon] = useState();
+  console.log(props);
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${slug}/`)
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemon(data);
+
+        console.log(data);
+      });
+  }, [slug]);
   return (
-    <div>
-      <h1 className="text-3xl">About</h1>
-    </div>
+    <>
+      {pokemon && (
+        <div className="w-3/12 m-auto bg-purple-100 mt-4 shadow-2xl flex justify-center flex-col items-center">
+          <h3 className="text-2xl text-green-900 uppercase">{pokemon.name}</h3>
+          <div className="flex justify-center">
+            <img className="w-48" src={pokemon.sprites["front_shiny"]} alt="" />
+            <img className="w-48" src={pokemon.sprites["back_shiny"]} alt="" />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import About from "./About";
 const url = "https://pokeapi.co/api/v2/pokemon?offset=0";
 function App() {
@@ -7,25 +7,46 @@ function App() {
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
-      .then(({ results }) => setPokemons(results))
+      .then((data) => {
+        const results = data.results.map((pokemon, idx) => ({
+          ...pokemon,
+          idx: idx + 1,
+        }));
+        setPokemons(results);
+      })
       .catch((error) => console.log(error));
   }, []);
   return (
     <Router>
       <div className="bg-gray-100">
         <h1 className="text-2xl bg-blue-100 border-2 border-solid border-purple-500 border-opacity-5">
-          Pokemons
+          <Link to="/" exact="true">
+            Pokemons
+          </Link>
         </h1>
-        <div>
-          {pokemons.map((pokemon, idx) => (
-            <p className="border border-purple-500" key={idx}>
-              {pokemon.name}
-            </p>
-          ))}
+        <div className="w-full flex justify-center p-5 bg-blue-500 ">
+          <input
+            className="outline-none"
+            type="text"
+            placeholder="enter pokemon's name"
+          />
         </div>
+        {/* <div className="flex">
+          {pokemons.map((pokemon) => (
+            <Link
+              to={`/about/${pokemon.idx}`}
+              className="border border-purple-500"
+              key={pokemon.idx}
+            >
+              {pokemon.name}
+            </Link>
+          ))}
+        </div> */}
       </div>
       <Switch>
-        <Route path="/about" component={About} />
+        <Route path="/about/:slug">
+          <About />
+        </Route>
       </Switch>
     </Router>
   );
